2578807d84d392fe37d48f1b782cf7963c58a953
Lospub <zhjyy002@gmail.com>
2024-12-12 12:31:39 -0700

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cocktailRoute from "./routes/cocktailRoute.js";
+import cors from "cors";
 const app = express();
 const PORT = 8080;
 
 app.use(express.static("public"));
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use("/cocktails", cocktailRoute);
